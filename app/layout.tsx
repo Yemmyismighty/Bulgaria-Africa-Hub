f70fd8ea49f0ffe17759c95c7c628ef6b4b5258d
Adeyemi Akitoye <143087999+Yemmyismighty@users.noreply.github.com>
2025-09-13 23:17:05 +0100

    Included Favicons to facilitate browser icon and branding.

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,6 +12,15 @@ export const metadata: Metadata = {
   title: 'Bulgaria Africa Hub',
   description: 'Connecting Bulgaria and Africa for a Brighter Future',
   generator: 'Adeyemi Akitoye',
+  icons: {
+    icon: [
+      { url: "/favicon_io/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon_io/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon_io/favicon.ico", type: "image/x-icon" },
+    ],
+    apple: "/favicon_io/apple-touch-icon.png",
+  },
+  manifest: "/favicon_io/site.webmanifest",
 }
 
 export default function RootLayout({
